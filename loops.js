b9309e47e5b0bd7d46ded996b9d1d1d504bdc03a
srinath ragalla <srinathragalla17@gmail.com>
2021-12-16 01:45:17 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,27 @@
+var a=6;
+for(let i=1; i<=a; i++){
+console.log("0")
+}
+
+
+
+var x=5;
+for(let i=1; i<=x; i++){
+console.log(i)
+}
+
+
+
+var sum=1;
+var n=10;
+for (let i=1; i<=n; i++){
+    sum +=i;
+}
+console.log(sum)
+
+let i=5;
+n=6;
+while(i<=n) {
+    console.log(i)
+    i++;
+}
